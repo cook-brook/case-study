3f01d03479381f69157c6d0ed090c54f13027ce9
cook-brook <72130874+cook-brook@users.noreply.github.com>
2021-05-26 12:27:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+Webcam.set({
+    width:310,
+    heightl:300,
+    image_format:'png',
+    png_quality:90
+
+
+})
+camera=document.getElementById("camera");
+
+Webcam.attach('camera');
+
+function take_snapshot(){
+
+
+    }
